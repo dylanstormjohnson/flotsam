d857346d06c0ae0dd19cbe5745bb287a71eda988
Storm <dylanstormjohnson@comcast.net>
2023-07-27 18:59:32 -0500

    Added typeDefs.js with help from Caleb

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,11 @@ const typeDefs = gql`
 
   scalar Date
 
+  type UserStory {
+    story: Story
+    endings: [storySlide]
+  }
+
   type User {
     _id: ID!
     firstName: String
@@ -12,6 +17,28 @@ const typeDefs = gql`
     password: String!
     createdAt: String
     updatedAt: String
+    playedStories: [UserStory]
+  }
+
+  type storyOption {
+    _id: ID!
+    text: String!
+    nextStorySlide: storySlide
+  }
+
+  type storySlide {
+    _id: ID!
+    text: String!
+    backgroundImage: String!
+    options: [storyOption]
+    endSlide: Boolean
+  }
+
+  type Story {
+    _id: ID!
+    name: String!
+    numberOfPossibleEndings: Int!
+    firstStorySlide: storySlide
   }
 
   type Auth {
