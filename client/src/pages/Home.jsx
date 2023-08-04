c1d34028d6adf19f90989330952a9eaa84239632
NoemiCouch <Noemicouch@gmail.com>
2023-08-03 21:49:16 -0500

    worked on Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,11 +16,13 @@ export default function Home() {
       isProtected={false}
       headContent={headContent}
     >
-      <div>Home</div>
-      {/* remove this button later */}
+      <div className="homeContent">
+        <h1>Sailing Close to the Wind</h1>
+        <p>Jack is pushing his luck driving that car to the local garage on his own. The authorities suspended his license six months ago, and if you ask me, he is sailing close to the wind with that idea.</p>
       <Link to="/gameplay/000000000000">
         <Button>Play Game</Button>
       </Link>
+      </div>
     </Page>
   );
 }
