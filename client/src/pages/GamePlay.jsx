99230b1fb9d93df2a71a3c57ae3e694bfb7d959f
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-08-05 12:51:38 -0500

    convert audio from rest to use with graphQL

--- client/src/pages/GamePlay.jsx
+++ client/src/pages/GamePlay.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_STORY, QUERY_SINGLE_STORY_SLIDE, QUERY_SINGLE_STORY_OPTION } from '../graphql/queries';
 // import { audio } from '.../server/utils/audioAPI.js'
-
+import { useEffect } from "react";
+import audio from '../utils/audioAPI'
 const headContent = (
   <>
     <title>FlotSam - GamePlay</title>
@@ -12,26 +13,44 @@ const headContent = (
   </>
 );
 
+const songExample = '16154'
+
 export default function GamePlay() {
   const { gameId } = useParams();
 
-  const { loading, error, data } = useQuery(QUERY_SINGLE_STORY, {
-    variables: { storyId: gameId },
-  });
+  // const { loading, error, data } = useQuery(QUERY_SINGLE_STORY, {
+  //   variables: { storyId: gameId },
+  // });
+
+  const getAudio = async (song) => {
+    try {
+
+      const data = await audio(song);
+
+      console.log(data);
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getAudio(songExample)
+  }, []);
 
   // const { hloading, herror, hdata } = useQuery(QUERY_SINGLE_STORY_SLIDE, {
   //   variables: { storyId: gameId },
   // });
 
-  console.log(data)
+  // console.log(data)
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error: {error.message}</p>;
 
   return (
     // add isProtected to this line
     <Page className="authContainer"  headContent={headContent}>
-      <h1>Welcome to {data.story.name}</h1>
+      {/* <h1>Welcome to {data.story.name}</h1> */}
     </Page>
   );
 }
