738ddfa7e2fd5d82bd22981446fce3306c098c0c
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-08-05 20:13:22 -0500

    adding audio to game page

--- client/src/pages/GamePlay.jsx
+++ client/src/pages/GamePlay.jsx
@@ -2,14 +2,12 @@ import Page from "../components/Page";
 import { useParams } from "react-router-dom";
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_STORY, QUERY_SINGLE_STORY_SLIDE, QUERY_SINGLE_STORY_OPTION } from '../graphql/queries';
-// import { audio } from '.../server/utils/audioAPI.js'
 import { useEffect } from "react";
 import audio from '../utils/audioAPI'
 const headContent = (
   <>
     <title>FlotSam - GamePlay</title>
     <meta name="description" content="This is the game play page of my app." />
-   {/* <audio>${audio}</audio> */}
   </>
 );
 
@@ -17,10 +15,6 @@ const songExample = '16154'
 
 export default function GamePlay() {
   const { gameId } = useParams();
-
-  // const { loading, error, data } = useQuery(QUERY_SINGLE_STORY, {
-  //   variables: { storyId: gameId },
-  // });
 
   const getAudio = async (song) => {
     try {
@@ -38,19 +32,8 @@ export default function GamePlay() {
     getAudio(songExample)
   }, []);
 
-  // const { hloading, herror, hdata } = useQuery(QUERY_SINGLE_STORY_SLIDE, {
-  //   variables: { storyId: gameId },
-  // });
-
-  // console.log(data)
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error: {error.message}</p>;
-
   return (
-    // add isProtected to this line
     <Page className="authContainer"  headContent={headContent}>
-      {/* <h1>Welcome to {data.story.name}</h1> */}
     </Page>
   );
 }
