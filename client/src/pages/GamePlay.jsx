089ad9831ab02959ecc54c6864a747912aab6c89
NoemiCouch <Noemicouch@gmail.com>
2023-08-05 20:50:49 -0500

    adding style to gameplay elements

--- client/src/pages/GamePlay.jsx
+++ client/src/pages/GamePlay.jsx
@@ -1,15 +1,18 @@
 import Page from "../components/Page";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useQuery, useLazyQuery } from '@apollo/client';
-import { QUERY_SINGLE_STORY, QUERY_SINGLE_STORY_SLIDE } from '../graphql/queries';
+import { useQuery, useLazyQuery } from "@apollo/client";
+import {
+  QUERY_SINGLE_STORY,
+  QUERY_SINGLE_STORY_SLIDE,
+} from "../graphql/queries";
 // import { audio } from '.../server/utils/audioAPI.js'
-import spinner from '../assets/images/loadingSpinner/spinner.gif';
+import spinner from "../assets/images/loadingSpinner/spinner.gif";
 const headContent = (
   <>
     <title>FlotSam - GamePlay</title>
     <meta name="description" content="This is the game play page of my app." />
-   {/* <audio>${audio}</audio> */}
+    {/* <audio>${audio}</audio> */}
   </>
 );
 
@@ -18,24 +21,30 @@ export default function GamePlay() {
   const [storyInfo, setStoryInfo] = useState(null);
   const [currentSlide, setCurrentSlide] = useState(null);
 
-  const { loading: storyLoading, error: storyError, data: storyData } = useQuery(QUERY_SINGLE_STORY, {
+  const {
+    loading: storyLoading,
+    error: storyError,
+    data: storyData,
+  } = useQuery(QUERY_SINGLE_STORY, {
     variables: { storyId: gameId },
   });
 
   useEffect(() => {
-    if(!storyData) return;
+    if (!storyData) return;
 
     setCurrentSlide({
-      ...storyData.story.firstStorySlide
+      ...storyData.story.firstStorySlide,
     });
 
     setStoryInfo({
-      ...storyData.story
+      ...storyData.story,
     });
+  }, [storyData]);
 
-  }, [storyData])
-
-  const [getSlide, { loading: slideLoading, error: slideError, data: currentSlideData }] = useLazyQuery(QUERY_SINGLE_STORY_SLIDE);
+  const [
+    getSlide,
+    { loading: slideLoading, error: slideError, data: currentSlideData },
+  ] = useLazyQuery(QUERY_SINGLE_STORY_SLIDE);
 
   const handleButtonClick = async (slideId) => {
     const { data } = await getSlide({
@@ -45,30 +54,38 @@ export default function GamePlay() {
     });
 
     setCurrentSlide({
-      ...data.storySlide
-    })
+      ...data.storySlide,
+    });
   };
 
-  console.log(storyInfo)
-  console.log(currentSlide)
+  console.log(storyInfo);
+  console.log(currentSlide);
 
-  return(
-    <Page className="authContainer bg-primary"  headContent={headContent}>
-    {storyInfo ? (
-      <div className="gameSpace d-flex align-items-center flex-column">
-        <h1 className="gameTitle">{storyInfo?.name}</h1>
-        <div className="gameContainer">
+  return (
+    <Page className="authContainer gamePlayCon" headContent={headContent}>
+      {storyInfo ? (
+        <div className="gameSpace d-flex align-items-center flex-column">
+          <h1 className="gameTitle">{storyInfo?.name}</h1>
+          {/* <div className="gameContainer">
           <img src={storyInfo?.backgroundImage} alt="Background" />
+        </div> */}
+          <p className="textContainer">{currentSlide?.text}</p>
+          <div className="optionsContainer d-flex align-items-center flex-column">
+            {currentSlide?.options.map((options) => (
+              <button
+                className="options btn btn-outline-light w-100 mb-2"
+                onClick={() => handleButtonClick(options.nextStorySlide._id)}
+                key={options._id}
+              >
+                {options.text}
+              </button>
+            ))}
+          </div>
         </div>
-        <p className="textContainer">{currentSlide?.text}</p>
-        <div className="optionsContainer d-flex align-items-center flex-column">
-          {currentSlide?.options.map((options) => (
-          <button className="options btn btn-outline-dark" onClick={() => handleButtonClick(options.nextStorySlide._id)} key={options._id}>{options.text}</button>
-          ))}
-        </div>
-      </div>
       ) : null}
-      {storyLoading || slideLoading ? <img src={spinner} alt="loading" /> : null}
+      {storyLoading || slideLoading ? (
+        <img src={spinner} alt="loading" />
+      ) : null}
     </Page>
-  )
+  );
 }
