c96f2f895b90f6e8988748115f97b4a243bb9774
Storm <dylanstormjohnson@comcast.net>
2023-08-09 19:09:31 -0500

    C:/Program Files/Git/client/

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/'));
 })
 
 // Create a new instance of an Apollo server with the GraphQL schema
