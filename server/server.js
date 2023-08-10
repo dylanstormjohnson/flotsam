f379b679ddeeb9bf2d66338c115f8727703bf666
Storm <dylanstormjohnson@comcast.net>
2023-08-09 19:34:10 -0500

    client/build

--- server/server.js
+++ server/server.js
@@ -27,10 +27,10 @@ app.use(graphqlUploadExpress());
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   })
 }
 
