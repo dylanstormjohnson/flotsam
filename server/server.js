a41923314f216a83530e721403b0df87659ecb92
Storm <dylanstormjohnson@comcast.net>
2023-08-09 19:27:04 -0500

    dist

--- server/server.js
+++ server/server.js
@@ -27,12 +27,13 @@ app.use(graphqlUploadExpress());
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  })
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
